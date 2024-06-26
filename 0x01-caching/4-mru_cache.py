a81c7e33234cba191d9e9c9500afbc10c0fb6f3f
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-27 00:24:29 +0300

    MRU cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+LRU cache
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Least Recently Used cache
+    """
+
+    def __init__(self):
+        """
+        init method
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Add item to cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # Remove the least recently used item
+                lru_key = self.order.pop(0)
+                print(self.order)
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+
+    def get(self, key):
+        """
+        Get item from cache
+        """
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.insert(0, key)
+            return self.cache_data[key]
+        return None
